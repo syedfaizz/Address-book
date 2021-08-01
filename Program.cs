d82f14fa011e12537c576d6282ec354e9cea2ab7
syedfaizz <syedfaizz45@gmail.com>
2021-08-01 14:10:24 +0530

    [faisal] Add: contact-person

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactPerson contactPerson = new ContactPerson();
+            contactPerson.AddContact();
+        }
+    }
+}
